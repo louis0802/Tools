471fc404d35ab0bf1cf2529430bc07c779095b00
Louis <noimpossiblething0802@gmail.com>
2020-03-28 16:26:44 +0800

    Add TouchPad Gusture

---
+++ TouchPadGesture.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+if ! [ $(id -u) = 0 ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+#First add yourself to the "input" group and log out/log back in:
+
+sudo gpasswd -a $USER input
+#Install all pre-requisite packages:
+
+sudo apt install libinput-tools xdotool ruby
+#Install fusuma gem:
+
+sudo gem install fusuma
+#Create a configuration file as your user:
+
+mkdir -p ~/.config/fusuma
+echo "
+swipe:
+  3: 
+    left: 
+      command: 'xdotool key alt+Right'
+    right: 
+      command: 'xdotool key alt+Left'
+    up: 
+      command: 'xdotool key super+s'
+    down: 
+      command: 'xdotool key super+l'
+  4:
+    left: 
+      command: 'xdotool key super+Left'
+    right: 
+      command: 'xdotool key super+Right'
+    up: 
+      command: 'xdotool key super+a'
+    down: 
+      command: 'xdotool key super+s'
+pinch:
+  in:
+    command: 'xdotool key ctrl+plus'
+  out:
+    command: 'xdotool key ctrl+minus'
+
+threshold:
+  swipe: 1
+  pinch: 1
+
+interval:
+  swipe: 1
+  pinch: 1" >>  ~/.config/fusuma/config.yml
+
+sudo chmod 777 config.yml
+
+
+
+
+echo "[Desktop Entry]
+Type=Application
+Exec=/usr/local/bin/fusuma
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_SG]=fusuma
+Name=fusuma
+Comment[en_SG]=
+Comment=" >> ~/.config/autostart/fusuma.desktop
+
+sudo chmod 777 ~/.config/autostart/fusuma.desktop
+
